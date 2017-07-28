51d6392a0272f552f8ecef2ec62454f44e69d3d9
Ali <ar.k@outlook.com>
2017-07-27 18:37:04 -0700

    Add onClick event handler to the button

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,18 +5,29 @@ import './Input.css'
 class Input extends Component {
     constructor() {
         super()
+
+        this.state = { newDate: ''};
+    }
+
+    showBirthday() {
+        console.log(this.state.newDate);
     }
 
     render() {
         return(
             <div className='input'>
-                <h4>Pick Your Birthday From Below Input Bar</h4>
+                <h4>Select Your Birthday From Below</h4>
                 <Form inline>
-                    <FormControl type="date"></FormControl>
+                    <FormControl 
+                    type="date"
+                    onChange={event => this.setState({newDate: event.target.value})} 
+                    >
+                    </FormControl>
                      {'  '} 
-                    <Button bsStyle="primary">Submit</Button>
+                    <Button bsStyle="primary" onClick={() => {this.showBirthday()}} >Submit</Button>
                 </Form>
             </div>
+
         )
     }
 }
